95fff2bb37592d805748d834aebbe60120263567
Denis Ferraz <denis_ferraz359@hotmail.com>
2024-02-18 20:51:05 -0300

    Add files via upload

---
+++ aut_excel_vba.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Teste</title>
+</head>
+<body><center>
+<fieldset>
+<legend>Validação de Tokens</legend>
+    <table border="1px" style="color:black">
+<tr>
+  <td>Hotel</td>
+  <td>RID</td>
+  <td>Validade</td>
+  <td>Token</td>
+</tr><tr>
+  <td>Novotel Salvador Hangar Aeroporto</td>
+  <td>H8185</td>
+  <td>31/12/2023</td>
+  <td>H_45_8185_13_ABC_8</td>
+</tr><tr>
+  <td>Ibis Salvador Hangar Aeroporto</td>
+  <td>H8181</td>
+  <td>31/12/2023</td>
+  <td>H_45_8181_13_ABC_8</td>
+</tr><tr>
+  <td>Novotel Salvador Rio Vermelho</td>
+  <td>HB275</td>
+  <td>31/07/2023</td>
+  <td>H_45_B275_13_ABC_8</td>
+</tr>
+
+    </table></fieldset></center>
+</body>
+</html>
